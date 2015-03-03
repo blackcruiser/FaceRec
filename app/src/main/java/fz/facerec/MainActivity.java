3be153b62ed89e0d2d50246481580cd58d90b4ec
blackcruiser <zmzxlw@163.com>
2015-03-03 23:09:00 +0800

    Add btnOpen callback function

--- app/src/main/java/fz/facerec/MainActivity.java
+++ app/src/main/java/fz/facerec/MainActivity.java
@@ -1,43 +1,96 @@
 package fz.facerec;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 
-public class MainActivity extends Activity{
+import java.io.FileNotFoundException;
+
+public class MainActivity extends Activity {
+
+    private Button btnOpen, btnDetect;
+    private ImageView imageView;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
-                case LoaderCallbackInterface.SUCCESS:
-                {
+                case LoaderCallbackInterface.SUCCESS: {
                     Log.i("onManagerConnected", "OpenCV loaded successfully");
 
-                } break;
-                default:
-                {
+                }
+                break;
+                default: {
                     super.onManagerConnected(status);
-                } break;
+                }
+                break;
             }
         }
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
 
+        btnOpen = (Button) findViewById(R.id.Button_Open);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.setType("image/*");
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
     @Override
-    protected  void onResume(){
+    protected void onResume() {
+        super.onResume();
+
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_10, this, mLoaderCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 0: {
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    ContentResolver resolver = getContentResolver();
+                    String fileType = resolver.getType(uri);
+                    String docId = DocumentsContract.getDocumentId(uri);
+                    String args = docId.split(":")[1];
+
+                    if (fileType.startsWith("image"))
+                    {
+                        Cursor cursor = resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Images.Media.DATA}, MediaStore.Images.Media._ID + "=?", new String[]{args}, null);
+                        cursor.moveToFirst();
+                        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                        imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+                    }
+                }
+            }
+            default:
+                break;
+
+        }
     }
 }
