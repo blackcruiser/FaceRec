38cecaee541d6d74619ada845930d998f6f8755e
blackcruiser <zmzxlw@163.com>
2015-03-04 18:21:48 +0800

    Add open image file function.

--- app/src/main/java/fz/facerec/MainActivity.java
+++ app/src/main/java/fz/facerec/MainActivity.java
@@ -4,26 +4,31 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-
-import java.io.FileNotFoundException;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
 
 public class MainActivity extends Activity {
 
-    private Button btnOpen, btnDetect;
+    private Button btnOpenFile, btnOpenCamera, btnDetect;
     private ImageView imageView;
+    private boolean isLoadLibSuccess = false;
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 
@@ -31,12 +36,20 @@ public class MainActivity extends Activity {
         public void onManagerConnected(int status) {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
-                    Log.i("onManagerConnected", "OpenCV loaded successfully");
+                    Toast toast = Toast.makeText(getApplicationContext(), "Load OpenCV success!", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.BOTTOM, 0, 0);
+                    toast.show();
 
+                    isLoadLibSuccess = true;
                 }
                 break;
                 default: {
-                    super.onManagerConnected(status);
+                    Toast toast = Toast.makeText(getApplicationContext(), "Fail to load OpenCV library!", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+
+                    isLoadLibSuccess = false;
+                    finish();
                 }
                 break;
             }
@@ -49,8 +62,10 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.main_activity);
 
-        btnOpen = (Button) findViewById(R.id.Button_Open);
-        btnOpen.setOnClickListener(new View.OnClickListener() {
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnOpenFile = (Button)findViewById(R.id.Button_OpenFile);
+        btnOpenFile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
@@ -59,6 +74,9 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent, 0);
             }
         });
+
+        btnOpenCamera = (Button)findViewById(R.id.Button_OpenCamera);
+        btnDetect = (Button)findViewById(R.id.Button_Detect);
     }
 
     @Override
@@ -76,21 +94,23 @@ public class MainActivity extends Activity {
                     Uri uri = data.getData();
                     ContentResolver resolver = getContentResolver();
                     String fileType = resolver.getType(uri);
-                    String docId = DocumentsContract.getDocumentId(uri);
-                    String args = docId.split(":")[1];
 
                     if (fileType.startsWith("image"))
                     {
-                        Cursor cursor = resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Images.Media.DATA}, MediaStore.Images.Media._ID + "=?", new String[]{args}, null);
+                        Cursor cursor = resolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new String[]{MediaStore.Images.Media.DATA}, null, null, null);
                         cursor.moveToFirst();
                         String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-                        imageView.setImageBitmap(BitmapFactory.decodeFile(path));
+                        Mat img = Highgui.imread(path);
+                        //TODO
+                        imageView.setImageURI(uri);
                     }
                 }
+                break;
             }
             default:
                 break;
 
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
