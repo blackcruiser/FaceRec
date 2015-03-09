63f6ddf5e7cc6fdd8f136b1b7cf164551e9d81b8
blackcruiser <zmzxlw@163.com>
2015-03-09 18:15:23 +0800

    try to deal with onPreviewFrame function: transfer raw camera data to opencv Mat

--- app/src/main/java/fz/facerec/CameraActivity.java
+++ app/src/main/java/fz/facerec/CameraActivity.java
@@ -2,20 +2,34 @@ package fz.facerec;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class CameraActivity extends Activity
 {
 	private Button btnBack, btnDetect;
 	private SurfaceView surfaceView;
+	private ImageView imageView;
 
 	private FaceRec faceRec;
 	private Camera camera;
@@ -28,6 +42,12 @@ public class CameraActivity extends Activity
 			camera = Camera.open();
 			Camera.Parameters cameraParams = camera.getParameters();
 			camera.setDisplayOrientation(90);
+			camera.setPreviewCallback(new PreviewCallback());
+			cameraParams.setPreviewFormat(ImageFormat.NV21);
+
+
+
+			camera.setParameters(cameraParams);// 设置相机参数
 
 			try
 			{
@@ -52,9 +72,8 @@ public class CameraActivity extends Activity
 			Camera.Parameters cameraParams = camera.getParameters();
 
 			//consider the display orientation
-			cameraParams.setPreviewSize(480, 640); // 设置预览图像大小
-			cameraParams.setPreviewFormat(ImageFormat.JPEG);
-			camera.setParameters(cameraParams);// 设置相机参数
+			cameraParams.setPreviewSize(width, height); // 设置预览图像大小
+			camera.setParameters(cameraParams);
 			camera.startPreview();// 开始预览
 		}
 	}
@@ -64,7 +83,18 @@ public class CameraActivity extends Activity
 		@Override
 		public void onPreviewFrame(byte[] data, Camera camera)
 		{
+			Camera.Parameters cameraParams = camera.getParameters();
+			Camera.Size size = cameraParams.getPreviewSize();
 
+			Mat mYuv = new Mat(size.height + size.height / 2, size.width, CvType.CV_8UC1);
+			mYuv.put(0, 0, data);
+			Mat mRGB = new Mat();
+			Imgproc.cvtColor(mYuv, mRGB, Imgproc.COLOR_YUV2BGRA_NV21, 4);
+			Bitmap bitmap = Bitmap.createBitmap(size.width, size.height, Bitmap.Config.ARGB_8888);
+			Utils.matToBitmap(mRGB, bitmap);
+			//Mat img = new Mat();
+			//Utils.bitmapToMat(bitmap, img);
+			imageView.setImageBitmap(bitmap);
 		}
 	}
 
@@ -79,6 +109,8 @@ public class CameraActivity extends Activity
         faceRec = (FaceRec)bundle.getSerializable("FaceRec");
 
         setContentView(R.layout.camera_layout);
+
+	    imageView = (ImageView)findViewById(R.id.imageView);
 
         surfaceView = (SurfaceView)findViewById(R.id.surfaceView);
 	    SurfaceHolder holder = surfaceView.getHolder();
